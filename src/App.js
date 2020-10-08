7dc6f5edd6cb953130466b32fabc170c4c2f4286
heykkyu <heykkyu@gmail.com>
2020-10-08 18:02:57 +0900

    knew how to use props at React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}) {
+  return <h1>I like {fav} </h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>안녕</div>
+      <Food fav="kimchi"  />
+      <Food fav="라면"  />
+      <Food fav="소고기"  />
+      <Food fav="돼지고기"  />
     </div>
   );
 }
