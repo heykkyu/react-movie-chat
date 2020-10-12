5bb05740e8b7e6b454889c00d2d81023da35371b
heykkyu <heykkyu@gmail.com>
2020-10-12 18:41:10 +0900

    learned setState

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h1>I like {name} </h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+        count: current.count +1,
+      })
+    );
+  }
+
+  minus = () => {
+    this.setState(current => ({
+        count: current.count -1,
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+         <h1>The number is : {this.state.count} </h1>
+         <button onClick={this.add}>add</button>
+         <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
-
-const foodILike = [
-  {
-    id:1,
-    name: '김치',
-    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
-    rating: 5,
-  },
-  {
-    id:2,
-    name: '김치2',
-    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
-    rating: 3,
-  },
-  {
-    id:3,
-    name: '김치3',
-    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
-    rating: 1,
-  },
-  {
-    id:4,
-    name: '김치4',
-    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
-    rating: 4.9,
-  },
-
-];
-
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 
 export default App;
