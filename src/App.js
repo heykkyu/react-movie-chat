8c2d538d075cddbcea963b75fd242eed302ecf13
heykkyu <heykkyu@gmail.com>
2020-10-12 17:18:11 +0900

    day2 - learned prop-types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({fav}) {
-  return <h1>I like {fav} </h1>;
+function Food({name, picture, rating}) {
+  return (
+    <div>
+      <h1>I like {name} </h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    id:1,
+    name: '김치',
+    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
+    rating: 5,
+  },
+  {
+    id:2,
+    name: '김치2',
+    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
+    rating: 3,
+  },
+  {
+    id:3,
+    name: '김치3',
+    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
+    rating: 1,
+  },
+  {
+    id:4,
+    name: '김치4',
+    image: 'https://www.fossmint.com/wp-content/uploads/2019/08/Free-Logo-Maker-and-Creator.png',
+    rating: 4.9,
+  },
+
+];
+
 
 function App() {
   return (
     <div>
-      <div>안녕</div>
-      <Food fav="kimchi"  />
-      <Food fav="라면"  />
-      <Food fav="소고기"  />
-      <Food fav="돼지고기"  />
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
