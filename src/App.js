fb1275f69c077ef5c2f727907ea348b3a40f9e65
heykkyu <heykkyu@gmail.com>
2020-10-21 22:51:14 +0900

    grid-template-columns: repeat(2, minmax(400px, 1fr));

--- src/App.js
+++ src/App.js
@@ -1,61 +1,8 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import Home from './routes/Home';
 import './App.scss';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    
-    console.log(movies)
-
-    this.setState({ movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render () {
-    const { isLoading, movies } = this.state;
-    const loading = isLoading ?
-      <div className="loader">
-        <div className="loader__text">
-          <img src="/loading.gif" alt="loading" title="loading"/>
-        </div>
-      </div>
-      : '';
-    return (
-      <section className="container">
-        {loading}
-        <div className="movies">
-        {movies.map((movie) => {
-          console.log(movie);
-          return ( 
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-            />
-          );
-        })}
-        </div>
-      </section>
-    ) 
-  }
+function App() {
+  return <Home />;
 }
-
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.scss';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    
+    console.log(movies)
+
+    this.setState({ movies, isLoading: false });
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render () {
+    const { isLoading, movies } = this.state;
+    const loading = isLoading ?
+      <div className="loader">
+        <div className="loader__text">
+          <img src="/loading.gif" alt="loading" title="loading"/>
+        </div>
+      </div>
+      : '';
+    return (
+      <section className="container">
+        {loading}
+        <div className="movies">
+        {movies.map((movie) => {
+          console.log(movie);
+          return ( 
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          );
+        })}
+        </div>
+      </section>
+    ) 
+  }
+}
+
+export default Home;
