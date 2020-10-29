eefb177c834a3ba789e6393ed6c691c7634fe547
heykkyu <heykkyu@gmail.com>
2020-10-29 22:33:41 +0900

    add navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" exact={true} component={About} />
     </Router>
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './About.scss';
 
 function About() {
-  return <span>About this page: I build it because I love movies.</span>;
+  return (
+    <div className="about__container">
+      <span>
+        "Freedom is the freedom to say~~~~"
+      </span>
+      <span>- George Orwell, 1984</span>
+    </div>
+  )
 }
 
 export default About;
