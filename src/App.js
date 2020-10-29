f2d58db36809f4884268997a9dc3668197a4ab2c
heykkyu <heykkyu@gmail.com>
2020-10-29 17:04:01 +0900

    react router hash to browser

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import Home from './routes/Home';
 import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+    </Router>
+  )
 }
 export default App;
